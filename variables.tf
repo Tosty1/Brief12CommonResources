59775378bca35faa72c5a2afb4fcb6d7f7e70b1b
CamilleDeSousaMathieu <62991139+Mellewanadoo@users.noreply.github.com>
2022-09-20 17:22:20 +0200

    mis a jour groupe storage

--- variables.tf
+++ variables.tf
@@ -1,3 +1,5 @@
+#Variables des ressources communes 
+
 variable "resource_group_name" {
 
   default = "Brief12CommonResources"
@@ -9,3 +11,22 @@ variable "resource_group_location" {
   default = "westeurope"
 
 }
+
+#Variables pour le groupe Storage
+variable "storage_container_name" {
+    default = "Brief12StorageContainer"
+}
+
+variable "storage_account_name" {
+    default = "Brief12StorageAccount"
+}
+
+#variable pour le groupe service bus
+
+
+
+
+#variable pour le groupe envoi de mail
+
+
+#variable pour le groupe MariaDB
