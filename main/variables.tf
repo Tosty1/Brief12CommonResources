2bd88fbcde766802802c594e126037f8dc0404b8
loicjon <69626784+loicjon@users.noreply.github.com>
2022-09-30 15:33:03 +0200

    Push PP

---
+++ main/variables.tf
@@ -0,0 +1,45 @@
+# serviceBus
+
+# variable pour le groupe service bus
+
+variable "servicebus_name" {
+
+  default = "brief12-ServiceBus"
+  type    = string
+
+}
+
+# Variable des Queues 
+
+variable "queue_count" {
+
+  default     = 10
+  type        = number
+  description = "Queue number"
+
+}
+
+variable "queue_prod" {
+
+  default     = "Prod"
+  type        = string
+  description = "Queue names"
+}
+
+
+variable "queue_dev" {
+
+  default     = "Dev"
+  type        = string
+  description = "Queue names"
+}
+
+# Variables des autorisations
+
+variable "autorisation_count" {
+
+  default     = 10
+  type        = number
+  description = "Autorisation number"
+
+}
