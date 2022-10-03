1613e735d9bfa83bfde05a90951880024be6a887
H0B0KEN <sarrejeremy.info@gmail.com>
2022-10-03 16:12:51 +0200

    V1.mariadb

--- main/data.tf
+++
@@ -1,7 +0,0 @@
-data "http" "myip" {
-  url = "http://ifconfig.me/ip"
-}
-
-data "http" "downloadcert" {
-  url = "https://cacerts.digicert.com/BaltimoreCyberTrustRoot.crt.pem"
-}
--- main/main.tf
+++
@@ -1,7 +0,0 @@
-
-# Creation d'un groupe de ressource
-
-resource "azurerm_resource_group" "p20cloud" {
-  name     = var.resource_group_name
-  location = var.resource_group_location
-}
