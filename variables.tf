b44ddbf9acf7c027110c7a40152f996d2b03e056
CamilleDeSousaMathieu <62991139+Mellewanadoo@users.noreply.github.com>
2022-10-05 16:08:18 +0200

    storage ok avec toutes les variables!

---
+++ variables.tf
@@ -0,0 +1,92 @@
+# CommonResources
+variable "resource_group_name" {
+  type    = string
+  default = "Brief12CommonResources"
+}
+
+variable "resource_group_location" {
+  type    = string
+  default = "westeurope"
+}
+
+#Variables pour le groupe Mariadb
+variable "resource_pfx" {
+  type    = string
+  default = "b12paaaa"
+}
+
+variable "admin_login" {
+  type    = string
+  default = "adminLogin1"
+}
+
+variable "database_count" {
+  type    = number
+  default = 10
+}
+
+#Variables pour le groupe Storage
+
+variable "storage_account_name" {
+    default = "brief12storageaccount"
+    type    = string
+}
+
+variable "container_name_pfx" {
+  description = "Containers Names"
+  default     = "groupe"
+  type        = string
+}
+
+variable "container_count" {
+  description = "Number of container"
+  default     = 10
+  type        = number
+}
+
+# serviceBus
+
+# variable pour le groupe service bus
+
+variable "servicebus_name" {
+
+  default = "brief12-ServiceBus"
+  type    = string
+
+}
+
+# Variable des Queues 
+
+variable "queue_count" {
+
+  default     = 10
+  type        = number
+  description = "Queue number"
+
+}
+
+variable "queue_prod" {
+
+  default     = "Prod"
+  type        = string
+  description = "Queue names"
+}
+
+
+variable "queue_dev" {
+
+  default     = "Dev"
+  type        = string
+  description = "Queue names"
+}
+
+# Variables des autorisations
+
+variable "autorisation_count" {
+
+  default     = 10
+  type        = number
+  description = "Autorisation number"
+
+}
+
